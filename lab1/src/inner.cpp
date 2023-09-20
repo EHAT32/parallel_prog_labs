398fa8ffc939026b424d7b9e55c414a63e57d996
EHAT32 <enatik14@gmail.com>
2023-09-20 23:20:34 +0300

    polished inner

--- lab1/src/inner.cpp
+++ lab1/src/inner.cpp
@@ -1,13 +1,13 @@
 #include <immintrin.h>
 #include <iostream>
 
-float inner(float* arr1,float* arr2,int n)
+float inner(float* arr1, float* arr2, int size)
 {
     int i = 0;
     float res_sse[4];
     float res = 0;
     __m128 sum = _mm_setzero_ps();
-    for(;i<n-3;i+=4)
+    for(; i < size - 3; i += 4)
     {
         __m128 x = _mm_load_ps(&arr1[i]);
         __m128 y = _mm_load_ps(&arr2[i]);
@@ -15,7 +15,7 @@ float inner(float* arr1,float* arr2,int n)
         sum = _mm_add_ps(sum, mul);
 
     }
-    for (;i<n;i++)
+    for (; i < size; i++)
     {
         res+=arr1[i]*arr2[i];
     }
@@ -25,12 +25,16 @@ return res;
 }
 
 int main(){
-    int size = 3;
+    int size = 5;
     float a[size], b[size];
     for (int i = 0; i < size; i++) {
         a[i] = i * i;
         b[i] = i;
     }
     float inn = inner(a, b, size);
+    std::cout << inn << std::endl;
     return 0;
 }
+//0, 1, 4, 9, 16
+//0, 1, 2, 3, 4
+//0+1+8+27+64
