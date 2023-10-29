005af509cbe83f6e6f47aca7f8d378cbd460aa54
EHAT32 <enatik14@gmail.com>
2023-10-29 23:14:23 +0300

    polished new_philosopher

--- lab4/src/new_philosopher.cpp
+++ lab4/src/new_philosopher.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <thread>
 #include <mutex>
@@ -30,14 +31,16 @@ private:
 
     void think() {
         std::cout << "Philosopher " << id_ << " is thinking" << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     }
 
     void eat() {
         std::cout << "Philosopher " << id_ << " is hungry and trying to grab forks" << std::endl;
         std::scoped_lock lock(left_fork_.mutex_, right_fork_.mutex_);
         std::cout << "Philosopher " << id_ << " is eating" << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+        std::cout << "Philosopher " << id_ << " has finished eating" << std::endl;
+        // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 };
 
@@ -45,7 +48,6 @@ int main() {
     const int num_philosophers = 5;
     std::array<Fork, num_philosophers> forks;
     std::thread threads[num_philosophers];
-
 
     // create philosophers
     for (int i = 0; i < num_philosophers; ++i) {
