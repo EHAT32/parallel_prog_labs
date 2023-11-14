f94b3bcf4eac15d19021324f5ef1ba86afc60986
Roman <enatik14@gmail.com>
2023-11-14 14:28:44 +0300

    minor fix

--- lab5/src/extremum.cpp
+++ lab5/src/extremum.cpp
@@ -7,6 +7,7 @@ template<typename T>
 T findMax(const std::vector<T>& vec){
     T maxVal = vec[0];
     #pragma omp parallel for
+    {
         for (int i = 0; i < vec.size(); i++) {
             #pragma omp critical
                 {
@@ -15,6 +16,7 @@ T findMax(const std::vector<T>& vec){
                     }
                 }
         }
+    }
     return maxVal;
 }
 
