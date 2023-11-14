0370ec0d288fed23b39b1c70c1d740ba1efc4ebe
Roman <enatik14@gmail.com>
2023-11-14 16:11:03 +0300

    added joinNode

--- lab7/src/main.cpp
+++ lab7/src/main.cpp
@@ -2,16 +2,23 @@
 #include <oneapi/tbb/flow_graph.h>
 #include <string>
 #include <tbb/tbb.h>
+#include <tuple>
 
 void createGraph(tbb::flow::graph& graph){
-    //nodes
-    tbb::flow::function_node<int, std::string> upperNode(graph, tbb::flow::unlimited,
-    [](const int& n) -> std::string {std::cout << "Upper node received " << n << std::endl;
-    return std::to_string(n);});
-
-    tbb::flow::function_node<int, std::string> lowerNode(graph, tbb::flow::unlimited,
-    [](const int& n) -> std::string {std::cout << "Lower node received " << n << std::endl;
-    return std::to_string(n);});
+    //upperNode
+    tbb::flow::function_node<int, int> upperNode(graph, tbb::flow::unlimited,
+    [](const int& n) -> int {std::cout << "Upper node received " << n << std::endl;
+    return n;});
+    //lowerNode
+    tbb::flow::function_node<int, int> lowerNode(graph, tbb::flow::unlimited,
+    [](const int& n) -> int {std::cout << "Lower node received " << n << std::endl;
+    return n;});
+    //joinNode
+    tbb::flow::join_node<std::tuple<int, int>, tbb::flow::queueing> joinNode(graph);
+    //edges
+    tbb::flow::make_edge(upperNode, tbb::flow::input_port<0>(joinNode));
+    tbb::flow::make_edge(lowerNode, tbb::flow::input_port<1>(joinNode));
+    
 }
 
 int main(){
