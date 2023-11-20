13e01c93115afdc9f9b6eb02a4c450a5638d1036
EHAT32 <enatik14@gmail.com>
2023-11-20 20:21:38 +0300

    some addition

--- lab7/src/main.cpp
+++ lab7/src/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <tbb/tbb.h>
 #include <tuple>
+#include <utility>
 
 void createGraph(){
     tbb::flow::graph graph;
@@ -21,7 +22,10 @@ void createGraph(){
     tbb::flow::make_edge(lowerNode, tbb::flow::input_port<1>(joinNode));
     //finalNode
     tbb::flow::function_node<std::tuple<int, int>, int> finalNode(graph, tbb::flow::unlimited,
-    [](const std::tuple<int, int>&) {std::cout << "Nodes finished working" << std::endl; return 0;});
+    [](const std::tuple<int, int>& tup) {
+        auto [f, s] = tup;
+        std::cout << "Nodes finished working: 2 * n1 + 4 * n2 = " << f + s << std::endl; return 0;
+    });
     //finaEdge
     tbb::flow::make_edge(joinNode, finalNode);
     upperNode.try_put(1);
