9d2a51cc1a1d29e212e684cace27ba8e7e44861d
Roman <enatik14@gmail.com>
2023-11-28 14:58:04 +0300

    finished

--- parAlgs/src/calculatePi.cpp
+++ parAlgs/src/calculatePi.cpp
@@ -1,5 +1,8 @@
+#include <errno.h>
 #include <iostream>
 #include <omp.h>
+#include <ostream>
+#include <vector>
 
 template<typename T>
 double trapInt(const T& function, const float& a = 0, const float& b = 1, const int& N = 100000){
@@ -11,7 +14,7 @@ double trapInt(const T& function, const float& a = 0, const float& b = 1, const 
         res += (function(x + i * h) + function(x + (i+1) * h)) / 2 * h;
     }
     double duration = omp_get_wtime() - startTime;
-    std::cout << "Linear time is: " << duration << " seconds" << std::endl;
+    // std::cout << "Linear time is: " << duration << " seconds" << std::endl;
     return duration;
 }
 
@@ -49,8 +52,9 @@ float OMPTrapInt(const T& function, const float& a = 0, const float& b = 1, cons
             }
         }
     }
-    std::cout << "OMP time is: " << omp_get_wtime() - startTime << " seconds" << std::endl;
-    return res;
+    double duration = omp_get_wtime() - startTime;
+    // std::cout << "OMP time is: " << duration << " seconds" << std::endl;
+    return duration;
 }
 
 template<typename T>
@@ -74,7 +78,7 @@ float OMPForTrapInt(const T& function, const float& a = 0, const float& b = 1, c
         }
     }
     double duration = omp_get_wtime() - startTime;
-    std::cout << "Linear time is: " << duration << " seconds" << std::endl;
+    // std::cout << "OMP for time is: " << duration << " seconds" << std::endl;
     return duration;
 }
 
@@ -83,8 +87,23 @@ float forPi(const float& x){
 }
 
 int main(){
-    std::cout << trapInt(forPi) << std::endl;
-    std::cout << OMPTrapInt(forPi) << std::endl;
-    std::cout << OMPForTrapInt(forPi) << std::endl;
+    int num_threads = 2;
+    omp_set_num_threads(num_threads);
+    int N = 1000;
+    double linTime = 0;
+    double forTime = 0;
+    std::cout << "Threads num: " << num_threads << std::endl;
+    for (int i = 0; i < N; i++) {
+        linTime += trapInt(forPi);
+        forTime += OMPForTrapInt(forPi);
+    }
+    linTime /= N;
+    forTime /= N;
+    std::cout << "Lin time: " << linTime << std::endl;
+    std::cout << "OMP For time: " << forTime  << std::endl;
+    float speedUp = linTime / forTime;
+    std::cout << "SpeedUp: " << speedUp << std::endl;
+    float efficiency = speedUp / num_threads;
+    std::cout << "Efficiency: " << efficiency << std::endl;
     return 0;
 }
